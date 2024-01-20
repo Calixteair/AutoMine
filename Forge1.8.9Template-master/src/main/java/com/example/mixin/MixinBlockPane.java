bba9f8d4421b2fc6fbe6668b9bfa463c877aecef
Calixte REYMOND <creymond@univ-fcomte.fr>
2024-01-20 10:18:56 +0100

    ff

---
+++ Forge1.8.9Template-master/src/main/java/com/example/mixin/MixinBlockPane.java
@@ -0,0 +1,26 @@
+package com.example.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPane;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public class MixinBlockPane {
+
+    @Inject(method = "getSelectedBoundingBox", at = @At("HEAD"), cancellable = true)
+    public void getSelectedBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos, CallbackInfoReturnable<AxisAlignedBB> callbackInfo) {
+        if (!(Block.class.cast(this) instanceof BlockPane)) {
+            return;
+        }
+        callbackInfo.setReturnValue(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D));
+    }
+}
